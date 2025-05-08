83121e7bd37fa7306740ff2b76c352570fe777e8
deemon1988 <dmn72835@gmail.com>
2025-05-09 00:18:05 +0300

    JS base

---
+++ public/script.js
@@ -0,0 +1,9 @@
+import { sayHello as say, helloUser } from "./modules/sayHello.js";
+import sumNumber from "./modules/sum.js";
+
+sumNumber(15, 5);
+
+export let userName = "Tom";
+say("Alex");
+say()
+helloUser();
