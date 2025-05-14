7462761d15f23532c4d2011be2514a01139ef2cb
deemon1988 <dmn72835@gmail.com>
2025-05-14 12:23:46 +0300

    Добавлены файлы уроков из другой ветки

---
+++ modules/sayHello.js
@@ -0,0 +1,10 @@
+// import { userName } from "../script.js";
+
+// export function sayHello() {
+//     alert(`Hello ${userName}`);
+// }
+
+// export function helloUser() {
+//     alert("Hi, Bob");
+// }
+
