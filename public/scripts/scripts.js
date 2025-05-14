28805c416f5ed157ac6ffbcfc0cd4088440c47cd
deemon1988 <dmn72835@gmail.com>
2025-05-14 21:23:17 +0300

    пробный коммит после удаления папки node_modules

---
+++ public/scripts/scripts.js
@@ -0,0 +1,74 @@
+import axios from "../../node_modules/axios/dist/esm/axios.js"
+
+// const userBtn = document.getElementById("user-info")
+// userBtn.addEventListener("click", () => {
+//     const token = localStorage.getItem("token")
+//     getUserInfo(token)
+// })
+
+
+async function getUserInfo(token) {
+
+    const res = await fetch("https://5966e44c806d7811.mokky.dev/auth_me", {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+
+    const json = await res.json()
+    console.log(json)
+    return json
+}
+
+
+axios.get("https://5966e44c806d7811.mokky.dev/auth_me", {
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
+})
+    .then((res) => {
+        const userData = res.data
+        console.log(userData)
+
+        createUserElement(userData)
+    })
+
+
+function createUserElement(user) {
+    const login = user.login
+    const username = user.username
+    const email = user.email
+    const userInfo = [login, username, email]
+    const div = document.createElement("div")
+    const p = document.createElement("p")
+    p.innerHTML = `<h3>Привет, ${login}!</h3>`
+    const div_profile = document.createElement("div")
+    div_profile.innerHTML = "<strong>Профиль пользователя:</strong>"
+    div_profile.classList.add("user-profile")
+    div.appendChild(p)
+    userInfo.forEach(elem => {
+        if(elem){
+        const p = document.createElement("p")
+        p.textContent = `${elem}!`
+        div_profile.appendChild(p)
+        }
+
+    })
+
+    const header = document.getElementById("header")
+    header.after(div)
+    div.after(div_profile)
+    
+}
+
+axios.get("https://swapi-api.hbtn.io/api/films/1", {
+    
+})
+    .then((res) => {
+        const jsonData = res.data
+        console.log(jsonData)
+    })
+
